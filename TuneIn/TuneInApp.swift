73af354b6a19ef5720f4cd23a638721d3e3c62b0
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2023-01-23 15:16:58 -0600

    Initial Commit

---
+++ TuneIn/TuneInApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneInApp.swift
+//  TuneIn
+//
+//  Created by Hilly Yehoshua on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TuneInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
