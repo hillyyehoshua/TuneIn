c90da71505aa0852381227f4e6ebcb1ca8fe7e10
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2023-01-23 16:41:24 -0600

    Updating, making the log in front screen

--- TuneIn/ContentView.swift
+++ TuneIn/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+// Structure for the view that manages primary app screens and navigation.
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color ("Dark Blue")
+                .edgesIgnoringSafeArea(.all)
+            VStack (alignment: .center){
+                HStack{
+                    Text("TuneIn")
+                        .foregroundColor(.white)
+                        .font(.custom("Poppins-SemiBold", size: 58))
+                    Image(systemName:"play.circle.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                        .padding(.top, 25)
+                }
+                Spacer()
+                    .frame(height: 30)
+                HStack{
+                    Spacer()
+                    Text("Get Started by signing into your Spotify Account")
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 15)
+                        .font(.custom("Poppins-Regular", size: 20))
+                    Spacer()
+                }
+                
+                    
+                
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
@@ -24,3 +49,71 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+/*
+ import SwiftUI
+ import UIKit
+
+ // Structure for the view that manages primary app screens and navigation.
+ struct ContentView: View {
+     @EnvironmentObject var session: OnboardingVM
+     @State private var selection: Int = 0
+     @State private var addPostPresented = false
+     
+     init() {
+         UITabBar.appearance().backgroundColor = .white
+         UITabBar.appearance().unselectedItemTintColor = .black
+         UITabBar.appearance().itemPositioning = .centered
+     }
+     
+     var body: some View {
+         NavigationStack {
+             TabView(selection: $selection) {
+                 // Icon to navigate to home marketplace screen.
+                 HomeView(tabSelection: $selection)
+                     .tabItem {
+                         if (selection == 0) {
+                             Image(systemName: "house.fill")
+                         }
+                         else {
+                             Image(systemName: "house")
+                                 .environment(\.symbolVariants, .none)
+                         }
+                     }
+                     .tag(0)
+                 
+                 // Icon to navigate to main messaging screen.
+                 MainMessagesView()
+                     .tabItem {
+                         if (selection == 1) {
+                             Image(systemName: "message.fill")
+                         }
+                         else {
+                             Image(systemName: "message")
+                                 .environment(\.symbolVariants, .none)
+                         }
+                     }
+                     .tag(1)
+                 
+                 // Icon to navigate to profile view screen.
+                 ProfileView(tabSelection: $selection)
+                     .tabItem {
+                         if (selection == 2) {
+                             Image(systemName: "person.fill")
+                         }
+                         else {
+                             Image(systemName: "person")
+                                 .environment(\.symbolVariants, .none)
+                         }
+                     }
+                     .tag(2)
+             }
+             .accentColor(.black)
+             .navigationBarHidden(true)
+         }
+         .environmentObject(session)
+     }
+ }
+
+ */
---
+++ TuneIn/LogIn.swift
@@ -0,0 +1,20 @@
+//
+//  LogIn.swift
+//  TuneIn
+//
+//  Created by Hilly Yehoshua on 1/23/23.
+//
+
+import SwiftUI
+
+struct LogIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogIn_Previews: PreviewProvider {
+    static var previews: some View {
+        LogIn()
+    }
+}
