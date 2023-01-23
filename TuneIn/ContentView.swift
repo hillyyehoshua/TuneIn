178378c9334d6e49ebe7f21d909159fead3a9adc
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2023-01-23 17:31:27 -0600

    For Izzy to Join

--- TuneIn/ContentView.swift
+++ TuneIn/ContentView.swift
@@ -10,48 +10,8 @@ import SwiftUI
 // Structure for the view that manages primary app screens and navigation.
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color ("Dark Blue")
-                .edgesIgnoringSafeArea(.all)
-            VStack (alignment: .center){
-                HStack{
-                    Text("TuneIn")
-                        .foregroundColor(.white)
-                        .font(.custom("Poppins-SemiBold", size: 58))
-                    Image(systemName:"play.circle.fill")
-                        .foregroundColor(.white)
-                        .font(.system(size: 20))
-                        .padding(.top, 25)
-                }
-                Spacer()
-                    .frame(height: 30)
-                HStack{
-                    Spacer()
-                    Text("Get Started by signing into your Spotify Account")
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding([.leading, .trailing], 15)
-                        .font(.custom("Poppins-Regular", size: 20))
-                    Spacer()
-                }
-                Spacer()
-                    .frame(height: 22)
-                
-                Text("Log In with Spotify")
-                    .foregroundColor(.white)
-                    .font(.custom("Poppins-Regular", size: 18))
-                    .fixedSize(horizontal: false, vertical: true)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                    .frame(width: 210, height: 45)
-                    .background(RoundedRectangle(cornerRadius: 30).fill(Color ("Blue")).shadow(radius: 3))
-                
-                    
-                
-            }
-            .padding()
-        }
-        
+        Text("Hello World")
+
     }
 }
 
--- TuneIn/LogIn.swift
+++ TuneIn/LogIn.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct LogIn: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack{
+            Color ("Dark Blue")
+                .edgesIgnoringSafeArea(.all)
+            VStack (alignment: .center){
+                HStack{
+                    Text("TuneIn")
+                        .foregroundColor(.white)
+                        .font(.custom("Poppins-SemiBold", size: 58))
+                    Image(systemName:"play.circle.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                        .padding(.top, 25)
+                }
+                Spacer()
+                    .frame(height: 40)
+                HStack{
+                    Spacer()
+                    Text("Get Started by signing into your Spotify Account")
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 10)
+                        .font(.custom("Poppins-Regular", size: 19))
+                    Spacer()
+                }
+                Spacer()
+                    .frame(height: 26)
+                
+                Text("Log In with Spotify")
+                    .foregroundColor(.white)
+                    .font(.custom("Poppins-Regular", size: 19))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 230, height: 50)
+                    .background(RoundedRectangle(cornerRadius: 30).fill(Color ("Blue")).shadow(radius: 3))
+                
+                    
+                
+            }
+            .padding()
+        }
+            }
 }
 
 struct LogIn_Previews: PreviewProvider {
