6b8fd3ab6aa8c750555dcce18c91f63318b64a5d
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2023-01-23 17:25:46 -0600

    Login page

--- TuneIn/ContentView.swift
+++ TuneIn/ContentView.swift
@@ -34,6 +34,17 @@ struct ContentView: View {
                         .font(.custom("Poppins-Regular", size: 20))
                     Spacer()
                 }
+                Spacer()
+                    .frame(height: 22)
+                
+                Text("Log In with Spotify")
+                    .foregroundColor(.white)
+                    .font(.custom("Poppins-Regular", size: 18))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 210, height: 45)
+                    .background(RoundedRectangle(cornerRadius: 30).fill(Color ("Blue")).shadow(radius: 3))
                 
                     
                 
@@ -49,71 +60,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-/*
- import SwiftUI
- import UIKit
-
- // Structure for the view that manages primary app screens and navigation.
- struct ContentView: View {
-     @EnvironmentObject var session: OnboardingVM
-     @State private var selection: Int = 0
-     @State private var addPostPresented = false
-     
-     init() {
-         UITabBar.appearance().backgroundColor = .white
-         UITabBar.appearance().unselectedItemTintColor = .black
-         UITabBar.appearance().itemPositioning = .centered
-     }
-     
-     var body: some View {
-         NavigationStack {
-             TabView(selection: $selection) {
-                 // Icon to navigate to home marketplace screen.
-                 HomeView(tabSelection: $selection)
-                     .tabItem {
-                         if (selection == 0) {
-                             Image(systemName: "house.fill")
-                         }
-                         else {
-                             Image(systemName: "house")
-                                 .environment(\.symbolVariants, .none)
-                         }
-                     }
-                     .tag(0)
-                 
-                 // Icon to navigate to main messaging screen.
-                 MainMessagesView()
-                     .tabItem {
-                         if (selection == 1) {
-                             Image(systemName: "message.fill")
-                         }
-                         else {
-                             Image(systemName: "message")
-                                 .environment(\.symbolVariants, .none)
-                         }
-                     }
-                     .tag(1)
-                 
-                 // Icon to navigate to profile view screen.
-                 ProfileView(tabSelection: $selection)
-                     .tabItem {
-                         if (selection == 2) {
-                             Image(systemName: "person.fill")
-                         }
-                         else {
-                             Image(systemName: "person")
-                                 .environment(\.symbolVariants, .none)
-                         }
-                     }
-                     .tag(2)
-             }
-             .accentColor(.black)
-             .navigationBarHidden(true)
-         }
-         .environmentObject(session)
-     }
- }
-
- */
