c4168fb7971d88e156e4c4bd23a31f60417967ef
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2023-01-23 17:42:20 -0600

    update

---
+++ TuneIn/FindFriends.swift
@@ -0,0 +1,20 @@
+//
+//  FindFriends.swift
+//  TuneIn
+//
+//  Created by Hilly Yehoshua on 1/23/23.
+//
+
+import SwiftUI
+
+struct FindFriends: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindFriends_Previews: PreviewProvider {
+    static var previews: some View {
+        FindFriends()
+    }
+}
