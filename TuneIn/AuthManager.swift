8405c2efda14fb489a1a11384b69e926ec7bc972
Izzy Hood <izzy671@gmail.com>
2023-03-21 19:36:48 -0500

    Adding phone auth

--- TuneIn/AuthManager.swift
+++ TuneIn/AuthManager.swift
@@ -2,41 +2,7 @@
 //  AuthManager.swift
 //  TuneIn
 //
-//  Created by Izzy Hood on 3/7/23.
+//  Created by Izzy Hood on 3/20/23.
 //
 
 import Foundation
-import SwiftUI
-
-class AuthManager: ObservableObject {
-    @Published var isSignedIn = false
-    @Published var isAnonymous = false
-    @Published var isLoading = true
-    
-    var spotifySecret: String
-    var spotifyId: String
-    
-    struct Keys {
-        // For storage.
-        static let EXPIRATION = "EXPIRATION_DATE"
-        static let ACCESS_TOKEN = "ACCESS_TOKEN"
-        static let REFRESH_TOKEN = "REFRESH_TOKEN"
-    }
-    
-    init() {
-        
-        spotifyId = spotifyClientId
-        spotifySecret = spotifyClientSecretKey
-        
-        signIn()
-        //signOut() // Uncomment this to clear cache at the startup.
-    }
-    
-    public var signInURL: URL? {
-        let scope = "user-library-modify%20user-library-read"
-        let str = "\(SpotifyApi.AUTHENTICATE)?response_type=code&client_id=\(spotifyId)&scope=\(scope)&redirect_uri=\(SpotifyApi.REDIRECT)&show_dialog=TRUE"
-        return URL(string: str)
-    }
-    
-    
-}
---
+++ TuneIn/Views/ProfileCreationPhoneAuth.swift
@@ -0,0 +1,102 @@
+//
+//  ProfileCreationPhoneAuth.swift
+//  TuneIn
+//
+//  Created by Izzy Hood on 3/21/23.
+//
+
+import SwiftUI
+
+struct ProfileCreationPhoneAuth: View {
+    
+    @EnvironmentObject var dataManager: DataManager
+    @State var verCode = ""
+    @State var verificationComplete = false
+    @Binding var name: String
+    @Binding var usernm: String
+    @Binding var phoneNumber: String
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var btnBack : some View { Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+            Image("left") // set image here
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+            }
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Color("Dark Blue")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Please input the 6-digit verification code texted to your phone number")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .font(.custom("Poppins", size: 18))
+                
+                HStack(alignment: .center) {
+                    TextField("", text: $verCode)
+                        .modifier(PlaceholderStyle(showPlaceHolder: verCode.isEmpty, placeholder: "XXXXXX"))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .accentColor(.white)
+                        .font(.custom("Poppins-SemiBold", size: 36))
+                }
+                .frame(alignment: .center)
+                .foregroundColor(.white)
+                
+                if verCode.isEmpty{
+                    Text("Next")
+                        .foregroundColor(.white)
+                        .font(.custom("Poppins-Regular", size: 16))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 230, height: 50)
+                        .background(RoundedRectangle(cornerRadius: 30).fill(Color ("Grey")).shadow(radius: 3))
+                    
+                } else {
+                    
+                    NavigationLink(destination: ProfileCreationTimeZone(name: $name, usernm: $usernm, phoneNumber : $phoneNumber), isActive: $verificationComplete) {
+                        VStack {
+                            Button(
+                                "Next",
+                                action:{
+                                    AuthManager.shared.verifyCode(smsCode: verCode) { success in
+                                        if success {
+                                            verificationComplete = true
+                                            print("Code successfully verified")
+                                        } else {
+                                            print("Error, code could not be successfully verified")
+                                        }
+                                    }
+                                }
+                            )
+                            .foregroundColor(.white)
+                            .font(.custom("Poppins-Regular", size: 16))
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(width: 230, height: 50)
+                            .background(RoundedRectangle(cornerRadius: 30).fill(Color ("Blue")).shadow(radius: 3))
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
+    }
+}
+
+struct ProfileCreationPhoneAuth_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileCreationPhoneAuth(name: .constant("John Doe"), usernm: .constant("username"), phoneNumber: .constant("1234567890"))
+    }
+}
